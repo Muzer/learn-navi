42fa3a5afe35addd852428bae3d36099d766cdb1
mgillogly <mgillogly@4dd590c8-5356-56d4-01ec-4945be1ffd85>
2010-02-16 05:32:10 +0000

    Tagging Version 0.2, Build 42

---
+++ tags/Version 0.2.0 (Build 42) Beta/Classes/AppViewController.h
@@ -0,0 +1,31 @@
+//
+//  AppViewController.h
+//  Learn Navi iPhone App
+//
+//  Created by Michael Gillogly on 1/20/10.
+//  Copyright 2010 LearnNa'vi.org Community. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppViewController : UIViewController {
+	
+	UILabel *betaText;
+	UINavigationController *navController;
+	DictionaryTableViewController *dictionaryTableViewController;
+}
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navController;
+@property (nonatomic, retain) IBOutlet UILabel *betaText;
+@property (nonatomic, retain) IBOutlet UIViewController *dictionaryTableViewController;
+
+- (NSString *)versionString;
+- (NSString *)bundleShortVersionString;
+- (NSString *)bundleVersionNumber;
+
+- (IBAction) launchDictionary:(id)sender;
+- (IBAction) launchPhraseBook:(id)sender;
+- (IBAction) launchPractice:(id)sender;
+
+@end
